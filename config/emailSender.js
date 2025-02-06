765c8822f18579433ffdc17bb1f09b404b800396
Gayathri p s <gayathri07211@gmail.com>
2025-02-06 17:06:25 +0530

    Update emailSender.js

--- config/emailSender.js
+++ config/emailSender.js
@@ -1,10 +1,10 @@
-const nodemailer = require('nodemailer');
+import { createTransport } from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
+const transporter = createTransport({
   service: 'Gmail',
   auth: {
     user: 'gayathri07211@gmail.com', 
     pass: 'uldi flhq dovm vcuj'
   }
 });
-module.exports = transporter;
+export default transporter;
